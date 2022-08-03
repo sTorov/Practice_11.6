86ad1613599f6329c2edbe36dbc8772d7ce3b114
sTorov <sotorov2011@yandex.ru>
2022-08-03 22:15:04 +0300

    Добавлено описание для StringExtensions.cs

--- TelegramBot/Extensions/StringExtension.cs
+++ TelegramBot/Extensions/StringExtension.cs
@@ -4,6 +4,11 @@ namespace TelegramBot.Extensions
 {
     public static class StringExtension
     {
+        /// <summary>
+        /// Удаление лишних пробелов из сообщения и замена символа '.' на ','
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
         public static string RemoveUnnecessary(this string str)
         {
             str = Regex.Replace(str, @"\s+", " ");
