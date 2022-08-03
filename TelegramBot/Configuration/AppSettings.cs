8844dec1358066d50e97140502051afcbf94f241
sTorov <sotorov2011@yandex.ru>
2022-08-03 21:58:23 +0300

    Добавлены описания для AppSettings.cs

--- TelegramBot/Configuration/AppSettings.cs
+++ TelegramBot/Configuration/AppSettings.cs
@@ -1,7 +1,13 @@
 ﻿namespace TelegramBot.Configuration
 {
+    /// <summary>
+    /// Основные настройки
+    /// </summary>
     class AppSettings
     {
+        /// <summary>
+        /// Токен подключения к Telegram Bot API
+        /// </summary>
         public string? BotToken { get; set; }
     }
 }
